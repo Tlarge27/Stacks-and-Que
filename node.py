2dd4a4ebdbee116e0907bd03f20600ec9cd0becf
Tlarge27 <157741770+Tlarge27@users.noreply.github.com>
2025-09-22 03:17:02 +0000

    Final version

--- node.py
+++ node.py
@@ -1,3 +1,7 @@
 # Implement your Node class here
 class Node:
-    pass # delete this line
+    def __init__(self, value):
+        self.value = value 
+        self.next = none 
+
+#Line 4 is to store the customer name, line 5 is to point towards the next name in the queue 
